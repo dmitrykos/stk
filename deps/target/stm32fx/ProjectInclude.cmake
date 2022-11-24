9bfafd94c0d497fe7b8a2640e53bd1381ff81ca7
dmitrykos <dmitrykos@neutroncode.com>
2022-11-24 13:35:51 +0200

    Removed dependency on development board models and introduced TARGET_CPU_FAMILY to target specific CPU family instead. Build BSP package even when it is not used in linkage to validate its health.

--- deps/target/stm32fx/ProjectInclude.cmake
+++ deps/target/stm32fx/ProjectInclude.cmake
@@ -2,15 +2,6 @@ if (NOT ROOT_DIR)
     message(FATAL_ERROR "ROOT_DIR must be defined and point to the root of STK repository!")
 endif()
 
-# Base definitions
-add_definitions(
-    -DUSE_HAL_DRIVER
-    -DHSE_VALUE=8000000
-    -DOS_USE_TRACE_SEMIHOSTING_DEBUG
-    -DTRACE
-    -DUSE_FULL_ASSERT
-)
-
 # Base includes
 include_directories(        
     ${ROOT_DIR}/deps/target/stm32fx/include
@@ -21,28 +12,41 @@ include_directories(
 )
 
 # Device-specific includes
-if (BOARD_STM32F0DISCOVERY)
-    add_definitions(
-        -DSTM32F0
-        -DSTM32F051x8
-    )
+if (TARGET_CORTEX_M0)
+    if (NOT TARGET_CPU_FAMILY)
+        # Default CPU for STM32F0DISCOVERY board
+        set(TARGET_CPU_FAMILY STM32F051x8 CACHE STRING "TARGET_CPU_FAMILY" FORCE)
+    endif()
+    add_definitions(-DSTM32F0)
     include_directories(${ROOT_DIR}/deps/target/stm32fx/include/stm32f0-hal)
     link_directories(${ROOT_DIR}/deps/target/stm32fx/src/stm32f0-ld)
-elseif (BOARD_NUCLEOF103RB)
-    add_definitions(
-        -DSTM32F1
-        -DSTM32F103xB
-    )
+elseif (TARGET_CORTEX_M3)
+    if (NOT TARGET_CPU_FAMILY)
+        # Default CPU for NUCLEO-F103RB board
+        set(TARGET_CPU_FAMILY STM32F103xB CACHE STRING "TARGET_CPU_FAMILY" FORCE)
+    endif()
+    add_definitions(-DSTM32F1)
     include_directories(${ROOT_DIR}/deps/target/stm32fx/include/stm32f1-hal)
     link_directories(${ROOT_DIR}/deps/target/stm32fx/src/stm32f1-ld)
-elseif (BOARD_STM32F407DISC1)
-    add_definitions(
-        -DSTM32F4
-        -DSTM32F407xx
-    )
+elseif (TARGET_CORTEX_M4)
+    if (NOT TARGET_CPU_FAMILY)
+        # Default CPU for STM32F407DISC1 board
+        set(TARGET_CPU_FAMILY STM32F407xx CACHE STRING "TARGET_CPU_FAMILY" FORCE)
+    endif()
+    add_definitions(-DSTM32F4)
     include_directories(${ROOT_DIR}/deps/target/stm32fx/include/stm32f4-hal)
     link_directories(${ROOT_DIR}/deps/target/stm32fx/src/stm32f4-ld)
 endif()
+
+# Base definitions
+add_definitions(
+    -DUSE_HAL_DRIVER
+    -DHSE_VALUE=8000000
+    -DOS_USE_TRACE_SEMIHOSTING_DEBUG
+    -DTRACE
+    -DUSE_FULL_ASSERT
+    -D${TARGET_CPU_FAMILY}
+)
 
 # Device specific include file
 set(_STK_DEVICE_INC "cmsis_device.h" CACHE STRING "_STK_DEVICE_INC" FORCE)
