465250f3c9e8e7ed2c09e09e7a0e4cc42f00ed34
dmitrykos <dmitrykos@neutroncode.com>
2022-12-08 17:54:44 +0200

    Added Code Coverage reporting action.

--- coverage.sh
+++ coverage.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
 
+# 
+#  SuperTinyKernel: Minimalistic thread scheduling kernel for Embedded systems.
+# 
+#  Source: http://github.com/dmitrykos/stk
+# 
+#  Copyright (c) 2022 Dmitry Kostjucenko <dmitry.kostjucenko@gmail.com>
+#  License: MIT License, see LICENSE for a full text.
+# 
+
 # Example (assuming source directory stk is in home directory): 
-# ~/stk/coverage.sh -s ~/stk -b ~/stk-build -o ~/stk-coverage
+# ~/stk/coverage.sh -s ~/stk -b ~/stk-build -o ~/stk-coverage -f ~/stk-build/coverage.info
 
 SRC_DIR=""
 BUILD_DIR=""
 OUTPUT_DIR=""
+COVERAGE_FILE=""
 SHOW_IN_WEBBROWSER=false
 
 ShowHelp()
@@ -18,6 +28,7 @@ ShowHelp()
    echo "s DIR   Source directory."
    echo "b DIR   Build directory."
    echo "o DIR   Output directory (location of HTML report)."
+   echo "f PATH  Fullpath to the coverage info file including the filename."
    echo "w       Open generated HTML report in the FireFox web browser."
    echo
 }
@@ -25,30 +36,34 @@ ShowHelp()
 CheckArgs()
 {
     if [ -z "$SRC_DIR" ] ; then
-        echo "Failed: no source directory provided!" && exit;
+        echo "Failed: no source directory provided!" && exit 1
     fi
     if [ -z "$BUILD_DIR" ] ; then
-        echo "Failed: no build directory provided!" && exit;
+        echo "Failed: no build directory provided!" && exit 1
     fi
     if [ -z "$OUTPUT_DIR" ] ; then
-        echo "Failed: no output directory provided!" && exit;
+        echo "Failed: no output directory provided!" && exit 1
+    fi
+    if [ -z "$COVERAGE_FILE" ] ; then
+        echo "Failed: no coverage.info file and path provided!" && exit 1
     fi
 }
 
-while getopts ":hs:b:o:w" option; do
+while getopts ":hs:b:o:f:w" option; do
     case $option in
         h) ShowHelp && exit;;
         s) SRC_DIR=$OPTARG;;
         b) BUILD_DIR=$OPTARG;;
         o) OUTPUT_DIR=$OPTARG;;
+        f) COVERAGE_FILE=$OPTARG;;
         w) SHOW_IN_WEBBROWSER=true;;
-        \?) echo "Failed: invalid option!" && exit;;
+        \?) echo "Failed: invalid option! Option: $option" && exit 1;;
     esac
 done
 
-CheckArgs
+COVERAGE_FILE_DRAFT="$COVERAGE_FILE.draft"
 
-COVERAGE_FILE="$BUILD_DIR/coverage.info"
+CheckArgs
 
 # Configure
 cmake -S $SRC_DIR -B $BUILD_DIR -DCMAKE_BUILD_TYPE=Release -DBUILD_LIB=ON -DBUILD_TESTS=ON -DTEST_GENERIC=ON -DENABLE_COVERAGE=ON
@@ -66,15 +81,15 @@ ctest --test-dir $BUILD_DIR -C Release
 cmake --build $BUILD_DIR --target ExperimentalCoverage
 
 # Capture results
-lcov --capture --follow --directory $BUILD_DIR --output-file $COVERAGE_FILE
+lcov --capture --follow --directory $BUILD_DIR --output-file $COVERAGE_FILE_DRAFT
 
 # Cleanup (we need only STK)
-lcov --remove $COVERAGE_FILE "deps/*" "test/*" "/usr/*" --output-file "$COVERAGE_FILE.clean"
+lcov --remove $COVERAGE_FILE_DRAFT "*/deps/*" "*/test/*" "/usr/*" --output-file $COVERAGE_FILE
 
 # Produce HTML
-genhtml "$COVERAGE_FILE.clean" --output-directory $OUTPUT_DIR
+genhtml $COVERAGE_FILE --output-directory $OUTPUT_DIR
 
 # Open in browser
 if $SHOW_IN_WEBBROWSER ; then
-    firefox $OUTPUT_DIR/index.html
+    xdg-open $OUTPUT_DIR/index.html
 fi
