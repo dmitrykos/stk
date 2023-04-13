b6b439f62559726a2c3d74a0420f68e8688c10e5
dmitrykos <dmitrykos@neutroncode.com>
2023-04-13 13:12:26 +0300

    risc-v: Initial RISC-V support.

---
+++ deps/target/risc-v/src/init/semihosting.c
@@ -0,0 +1,205 @@
+/*
+ * SuperTinyKernel: Minimalistic thread scheduling kernel for Embedded systems.
+ *
+ * Source: http://github.com/dmitrykos/stk
+ *
+ * Copyright (c) 2022 Dmitry Kostjucenko <dmitry.kostjucenko@gmail.com>
+ * License: MIT License, see LICENSE for a full text.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <sys/stat.h>
+#include <time.h>
+
+/*! \def   __SEMIHOSTING_PRINTF_BUFFER
+    \brief printf() buffer size (bytes).
+*/
+#define SEMIHOSTING_PRINTF_BUFFER (128)
+
+// Details: https://wiki.segger.com/Semihosting
+#define SEMIHOSTING_SYS_OPEN            (0x01)
+#define SEMIHOSTING_SYS_CLOSE           (0x02)
+#define SEMIHOSTING_SYS_WRITEC          (0x03)
+#define SEMIHOSTING_SYS_WRITE0          (0x04)
+#define SEMIHOSTING_SYS_WRITE           (0x05)
+#define SEMIHOSTING_SYS_READ            (0x06)
+#define SEMIHOSTING_SYS_READC           (0x07)
+#define SEMIHOSTING_SYS_ISERROR         (0x08)
+#define SEMIHOSTING_SYS_ISTTY           (0x09)
+#define SEMIHOSTING_SYS_SEEK            (0x0A)
+#define SEMIHOSTING_SYS_FLEN            (0x0C)
+#define SEMIHOSTING_SYS_TMPNAM          (0x0D)
+#define SEMIHOSTING_SYS_REMOVE          (0x0E)
+#define SEMIHOSTING_SYS_RENAME          (0x0F)
+#define SEMIHOSTING_SYS_CLOCK           (0x10)
+#define SEMIHOSTING_SYS_TIME            (0x11)
+#define SEMIHOSTING_SYS_SYSTEM          (0x12)
+#define SEMIHOSTING_SYS_ERRNO           (0x13)
+#define SEMIHOSTING_SYS_GET_CMDLINE     (0x15)
+#define SEMIHOSTING_SYS_HEAPINFO        (0x16)
+#define SEMIHOSTING_EnterSVC            (0x17)
+#define SEMIHOSTING_ReportException     (0x18)
+#define SEMIHOSTING_SYS_ELAPSED         (0x30)
+#define SEMIHOSTING_SYS_TICKFREQ        (0x31)
+
+#define SEMIHOSTING_BREAKPOINT() __asm volatile("ebreak")
+
+/*! \brief     Call host.
+    \note      https://github.com/riscv-software-src/riscv-semihosting/blob/main/riscv-semihosting-spec.adoc
+    \return    Return value of the call.
+*/
+static inline __attribute__((always_inline)) int32_t smh_call_host(int32_t reason, const void *arg)
+{
+    register int32_t value __asm("a0") = reason;
+    register const void *parg __asm("a1") = arg;
+
+    __asm volatile(
+    " .option push          \n"
+    " .option norvc         \n" // slli, ebreak, srai "instructions must be 32-bit-wide instructions, they may not be compressed 16-bit instructions"
+    " .balign 16            \n"
+    " slli zero, zero, 0x1f \n" // entry NOP
+    " ebreak                \n" // break to debugger
+    " srai zero, zero, 0x7  \n" // NOP encoding the semihosting call number 7
+    " .option pop           \n"
+
+    : "=r" (value)            /* output */
+    : "0" (value), "r" (parg) /* input */
+    : "memory"                /* clobber */
+    );
+
+    return value;
+}
+
+static inline void smh_write0(const char *buf)
+{
+    smh_call_host(SEMIHOSTING_SYS_WRITE0, buf);
+}
+
+static inline void smh_writec(char c)
+{
+    smh_call_host(SEMIHOSTING_SYS_WRITEC, &c);
+}
+
+static inline int32_t smh_readc(void)
+{
+    return smh_call_host(SEMIHOSTING_SYS_READC, NULL);
+}
+
+int _open(const char *path, int oflag, ...)
+{
+    // TO-DO
+    SEMIHOSTING_BREAKPOINT();
+    return -1;
+}
+
+int _close(int fildes)
+{
+    // TO-DO
+    SEMIHOSTING_BREAKPOINT();
+    return -1;
+}
+
+ssize_t _read(int fildes, void* buf, size_t nbyte)
+{
+    // TO-DO
+    SEMIHOSTING_BREAKPOINT();
+    return -1;
+}
+
+ssize_t _write(int fildes, const void* buf, size_t nbyte)
+{
+    // TO-DO
+    SEMIHOSTING_BREAKPOINT();
+    return -1;
+}
+
+off_t _lseek(int fildes, off_t offset, int whence)
+{
+    // TO-DO
+    SEMIHOSTING_BREAKPOINT();
+    return -1;
+}
+
+int _fstat(int fildes, struct stat *buf)
+{
+    // TO-DO
+    SEMIHOSTING_BREAKPOINT();
+    return -1;
+}
+
+int _isatty(int fildes)
+{
+    // TO-DO
+    SEMIHOSTING_BREAKPOINT();
+    return -1;
+}
+
+pid_t _getpid(void)
+{
+    // TO-DO
+    SEMIHOSTING_BREAKPOINT();
+    return -1;
+}
+
+int _kill(pid_t pid, int sig)
+{
+    // TO-DO
+    SEMIHOSTING_BREAKPOINT();
+    return -1;
+}
+
+int _gettimeofday(struct timeval *tv, void *tz)
+{
+    // TO-DO
+    SEMIHOSTING_BREAKPOINT();
+    return -1;
+}
+
+int printf(const char *fmt, ...)
+{
+    char buffer[SEMIHOSTING_PRINTF_BUFFER] = {};
+
+    va_list va;
+    va_start(va, fmt);
+    int32_t ret = vsnprintf(buffer, sizeof(buffer) - 1, fmt, va);
+    va_end(va);
+    
+    smh_write0(buffer);
+    return ret;
+}
+
+int putchar(int c)
+{
+    smh_writec((char)c);
+    return c;
+}
+
+int getchar(void)
+{
+    return smh_readc();
+}
+
+time_t time(time_t *_timer)
+{
+    return smh_call_host(SEMIHOSTING_SYS_TIME, _timer);
+}
+
+__attribute__((noreturn)) void abort(void)
+{
+    //printf("abort\n");
+
+    for (;;)
+    {
+        __asm__ volatile(
+        "wfi                \n"
+        "nop                \n");
+    }
+}
+
+__attribute__((noreturn)) void __assert_func(const char *file, int line, const char *func, const char *expr)
+{
+    printf("assert failed:\n - file: %s\n - line: %d\n - function: %s\n - what: %s\n", file, line, func, expr);
+    abort();
+}
